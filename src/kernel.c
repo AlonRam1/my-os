2ff5522f1b742a28894f91c9346b3d7de3882f29
AlonRam1 <alon.ram.rimmler@gmail.com>
2026-06-25 16:14:37 +0300

    cleaned up code

--- src/kernel.c
+++ src/kernel.c
@@ -2,17 +2,22 @@
 
 void kmain(void)
 {
+    //0xB8000 = start of VGA text buffer in memory. coordinates on screen are mapped to addresses (memory mapped IO)	
     volatile uint16_t* vga = (volatile uint16_t*)0xB8000;
 
     const char* msg = "Hello kernel";
 
     for (uint32_t i = 0; i < 12; i++)
     {
-        vga[i] = (0x0F << 8) | msg[i];
+	//vga screen cell format:
+	//[[ background | foreground | character ]
+	//    4 bits        4 bits       8 bits
+        vga[i] = (0x0F << 8) | msg[i]; // now vga[i] is 0x0F00 (OR) msg[i](char=8 bits)
     }
 
     while (1)
     {
+	//__asm__ = GCC format for writing assembly    
         __asm__ volatile ("hlt");
     }
 }
